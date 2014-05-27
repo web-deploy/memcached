267b8d3cc7771e348503f627f7fa5cbdbbf9417e
Wayne Tsui <wtsui@tagged.com>
2014-05-27 15:43:12 -0700

    Define jenkins job in repo

---
+++ .jenkins/jobs/suite.dsl.groovy
@@ -0,0 +1,45 @@
+@GrabResolver('https://artifactory.tagged.com/artifactory/libs-release-local/')
+@Grab('com.tagged.build:jenkins-dsl-common:0.1.18')
+
+import com.tagged.build.common.*
+
+def memcached_project = new Project(
+    jobFactory,
+    [
+        githubOwner: 'jdi-tagged',
+        githubProject: 'memcached',
+        githubHost: 'github.com',
+        hipchatRoom:'eng',
+        email: 'jirwin@tagged.com'
+    ]
+)
+def memcached = memcached_project.downstreamJob {
+    jdk 'default'
+    label 'old-java && centos5'
+    steps{
+        shell '''
+			bash << _EOF_
+			./autogen.sh
+			./configure
+			make dist
+			rm -rf SOURCES SPECS BUILD BUILDROOT RPMS SRPMS
+			mkdir SOURCES
+			mv *tar.gz SOURCES
+			echo workspace "$WORKSPACE"
+			rpmbuild \
+			           --define "_topdir $WORKSPACE" \
+			           --define "release `date +%Y%m%d%H%M%S`" \
+			           -ba memcached.spec
+			_EOF_
+        '''
+    }
+    triggers {
+        githubPush()
+        scm('5 * * * *')
+    }
+    publishers {          // mailer(String recipients, String dontNotifyEveryUnstableBuildBoolean = false, String sendToIndividualsBoolean = false)
+        mailer(memcached_project.notifyEmail, true, true)
+        archiveArtifacts('RPMS/**/*.rpm')
+    }
+    hipchat(memcached_project.hipchatRoom, false)
+}
