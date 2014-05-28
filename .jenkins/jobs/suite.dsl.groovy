66aebded7ec47d3b5c416107a7891e8d6aed46b2
Wayne Tsui <wtsui@tagged.com>
2014-05-27 17:25:51 -0700

    Remove extra line at the end of  shell script

--- .jenkins/jobs/suite.dsl.groovy
+++ .jenkins/jobs/suite.dsl.groovy
@@ -18,20 +18,19 @@ def memcached = memcached_project.downstreamJob {
     label 'orc01'
     steps{
         shell '''
-bash << _EOF_
-./autogen.sh
-./configure
-make dist
-rm -rf SOURCES SPECS BUILD BUILDROOT RPMS SRPMS
-mkdir SOURCES
-mv *tar.gz SOURCES
-echo workspace "$WORKSPACE"
-rpmbuild \
-           --define "_topdir $WORKSPACE" \
-           --define "release `date +%Y%m%d%H%M%S`" \
-           -ba memcached.spec
-_EOF_
-        '''
+            bash << _EOF_
+            ./autogen.sh
+            ./configure
+            make dist
+            rm -rf SOURCES SPECS BUILD BUILDROOT RPMS SRPMS
+            mkdir SOURCES
+            mv *tar.gz SOURCES
+            echo workspace "$WORKSPACE"
+            rpmbuild \
+                       --define "_topdir $WORKSPACE" \
+                       --define "release `date +%Y%m%d%H%M%S`" \
+                       -ba memcached.spec
+            _EOF_'''
     }
     triggers {
         githubPush()
